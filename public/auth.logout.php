9c11687ee2defcf7889f99572d4a7dbd919672bc
George Sowers III <Trey@Georges-MacBook-Pro.local>
2016-03-31 10:45:32 -0500

    first commit

---
+++ public/auth.logout.php
@@ -0,0 +1,18 @@
+<?php 
+?>	
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<title>Logout Action</title>
+
+		<meta charset="utf-8">
+
+	</head>
+
+	<body>
+	</body>
+
+</html>
---
+++ public/users.create.php
@@ -0,0 +1,18 @@
+<?php 
+?>
+
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<title>User Signup</title>
+
+		<meta charset="utf-8">
+
+	</head>
+
+	<body>
+	</body>
+
+</html>
