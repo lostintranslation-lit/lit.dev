5e93f46e24d4317e65fb7b68b7d9dad1be391fd8
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-03-31 16:00:10 -0500

    created nav bar

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,16 +1,30 @@
-<?php  ?>
+<?php  
+
+
+
+
+?>
 <!DOCTYPE html>
 <html>
 
 	<head>
 
-		<title></title>
-
+		<title>navbar</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<meta charset="utf-8">
 
 	</head>
 
 	<body>
+		
+		<div id="navbar">
+			<ul>
+			  <li class="changeBackground"><a href="i-saw-the-sign.html">I saw the sign . . .</a></li>
+			  <li class="changeBackground"><a href="eat-here.html">Eat Here!</a></li>
+			  <li class="changeBackground"><a href="movies-for-500.html">Movies for $500</a></li>
+			</ul>
+		</div>
+		</div>
 	</body>
 
 </html>
