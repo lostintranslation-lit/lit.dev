649ee85538c3525e956f12d37085584a4b0c5998
Anna Morton <annajmorton@gmail.com>
2016-04-01 16:39:37 -0500

    modified database seeder/feeder file, removed html and add git keys to navbar

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -3,28 +3,16 @@
 
 
 
-?>
-<!DOCTYPE html>
-<html>
+?>		
 
-	<head>
+<div id="navbar">
+	<ul>
+	  <li class="changeBackground"><a href="/index.php">home sweet home</a></li>
+	  <li class="changeBackground"><a href="/ads.index.php?type=sign">i saw the sign . . .</a></li>
+	  <li class="changeBackground"><a href="/ads.index.php?type=tattoo">tell me the tail of your tattoo</a></li>
+	  <li class="changeBackground"><a href="/ads.edit.php">edit this yo!</a></li>
+	</ul>
+</div>
 
-		<title>navbar</title>
-		<link rel="stylesheet" type="text/css" href="css/main.css">
-		<meta charset="utf-8">
 
-	</head>
 
-	<body>
-		
-		<div id="navbar">
-			<ul>
-			  <li class="changeBackground"><a href="i-saw-the-sign.html">I saw the sign . . .</a></li>
-			  <li class="changeBackground"><a href="eat-here.html">Eat Here!</a></li>
-			  <li class="changeBackground"><a href="movies-for-500.html">Movies for $500</a></li>
-			</ul>
-		</div>
-		</div>
-	</body>
-
-</html>
