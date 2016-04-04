392b3c7fedee8779d57dae425866e146d392ed0c
George Sowers III <Trey@Georges-MacBook-Pro.local>
2016-04-04 11:45:40 -0500

    created login page, updated logout, navbar, auth and index

--- public/auth.login.php
+++ public/auth.login.php
@@ -1,4 +1,29 @@
 <?php 
+
+include_once "../bootstrap.php";
+require_once "../utils/Auth.php";
+require_once "../utils/Input.php";
+
+function pageController()
+{
+	if(Input::has('username') && Input::has('password')) {
+		$username = Input::get('username');
+		$password = Input::get('password');
+
+		$result = Auth::attempt($username, $password);
+
+		if($result) {
+			header("Location: authorized.php");
+			die();
+		}
+	}
+
+	$attemptedUsername = inputHas('username') ? inputGet('username') : '';
+
+	$attemptedPassword = inputHas('password') ? inputGet('password') : '';
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -10,9 +35,27 @@
 
 		<meta charset="utf-8">
 
+		<link rel="stylesheet" href="/css/main.css">
+		<?= $font_links;  ?>
+
 	</head>
 
 	<body>
+		<h1>Log In</h1>
+
+		<form method="POST">
+        	<label>Username</label>
+        	<input type="text" name="username" placeholder="Enter Username"><br>
+        	<label>Password</label>
+        	<input type="password" name="password" placeholder="Enter Password"><br>
+        	<input type="submit" name="submit">
+    	</form>
+
+    	<div class='flexbox page_content'>
+			<img src="/img/Luis_Pic/7.png" alt="Luis" style="width:400px;height:400px;"/>
+		<layer id="placeholderlayer"></layer><div id="placeholderdiv"></div>
+
+
 	</body>
 
 </html>
--- utils/Auth.php
+++ utils/Auth.php
@@ -1,1 +1,39 @@
 <?php
+
+class Auth
+{
+	public static $username = 'guest';
+	public static $password = '$2y$10$SLjwBwdOVvnMgWxvTI4Gb.YVcmDlPTpQystHMO2Kfyi/DS8rgA0Fm';
+	public static function attempt($username, $password)
+	{
+		if (password_verify($password, self::$password) && ($username == self::$username)){
+			$_SESSION['LOGGED_IN_USER'] = $username;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public static function check()
+	{
+		return (isset($_SESSION['LOGGED_IN_USER']));
+	}
+	public static function user()
+	{
+		$username = self::check()? $_SESSION['LOGGED_IN_USER'] : null;
+		return $username;
+	}
+	public static function logout()
+	{
+		$_SESSION = array();
+    	// If it's desired to kill the session, also delete the session cookie.
+    	// Note: This will destroy the session, and not just the session data!
+    	if (ini_get("session.use_cookies")) {
+	        $params = session_get_cookie_params();
+	        setcookie(session_name(), '', time() - 42000,
+	        	$params["path"], $params["domain"],
+	        	$params["secure"], $params["httponly"]);
+    	}
+	    // Finally, destroy the session.
+	    session_destroy();
+	}
+}
