249767eb99436fcf7257f9efafc8a5f369e1ae06
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-04-04 13:16:17 -0500

    changes to get luis's photo to rotate

--- public/index.php
+++ public/index.php
@@ -18,50 +18,38 @@ include_once '../bootstrap.php';
 				<!-- just changed the title temporarily for fun -->
 				<h1>Luis's Loco Lingo!</h1>
 			</div>
-		
+		<!-- <span class="random_image"></span> this did not quite work-->
 		<div class='flexbox page_content'>
-	<!-- this is one photo from the Luis_Pic file -->	
-		<img src="/img/Luis_Pic/7.png" alt="Luis" style="width:400px;height:400px;"/>
-	
-	<!-- this is example code for rotating photos (off the Internet) -->
-			<layer id="placeholderlayer"></layer><div id="placeholderdiv"></div>
 
-			<script>
+	<script>
 
-			var howOften = 2; //number of seconds before rotating
-			var current = 0; //start the counter at 0
-			var ns6 = document.getElementById&&!document.all; //detect netscape 6
+		var delay=50; //set 1/2 second delay
+		var curindex=0;
 
-			// place your images in the array elements here
-			var items = new Array();
-			    items[0]="<img src="/img/Luis_Pic/1.png" alt="Luis" style="width:400px;height:400px;"/>;
-		 	    items[1]="<img src="/img/Luis_Pic/1.png" alt="Luis" style="width:400px;height:400px;"/>;
-		 	    items[2]="<img src="/img/Luis_Pic/2.png" alt="Luis" style="width:400px;height:400px;"/>;
-		 	   	items[3]="<img src="/img/Luis_Pic/3.png" alt="Luis" style="width:400px;height:400px;"/>;
-		 	    items[4]="<img src="/img/Luis_Pic/4.png" alt="Luis" style="width:400px;height:400px;"/>;
-		 	    items[5]="<img src="/img/Luis_Pic/5.png" alt="Luis" style="width:400px;height:400px;"/>;
-			
-			function rotater() {
-			    document.getElementById("placeholder").innerHTML = items[current];
-			    current = (current==items.length-1) ? 0 : current + 1;
-			    setTimeout("rotater()",howOften*1000);
-			}
+		var randomimages=new Array();
 
-			function rotater() {
-			    if(document.layers) {
-			        document.placeholderlayer.document.write(items[current]);
-			        document.placeholderlayer.document.close();
-			    }
-			    if(ns6)document.getElementById("placeholderdiv").innerHTML=items[current]
-			        if(document.all)
-			            placeholderdiv.innerHTML=items[current];
+		for (n=0;n<29;n++)
+		{
+			randomimages[n] = "/img/Luis_Pic/" + (n+1) + ".png";
+		}
 
-			    current = (current==items.length-1) ? 0 : current + 1; 
-			    setTimeout("rotater()",howOften*1000);
-			}
-			window.onload=rotater;
+		document.write('<img name="defaultimage" src="'+randomimages[Math.floor(Math.random()*(randomimages.length))]+'" style="width:400px;height:400px;">');
+		//this did not work: $('.random_image').append('<img name="defaultimage" src="'+randomimages[Math.floor(Math.random()*(randomimages.length))]+'" style="width:400px;height:400px;">')
+				
+		function rotateimage()
+		{
+			if (curindex==(tempindex=Math.floor(Math.random()*(randomimages.length)))){
+				curindex=curindex==0? 1 : curindex-1;
+			} else {
+				curindex=tempindex;
+				document.images.defaultimage.src=randomimages[curindex];
+				}			
+		}
+		
+		setInterval("rotateimage()",delay);
 
-			</script>
+	</script>
+
 		</div>
 	</body>
 
