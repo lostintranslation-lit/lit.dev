46ff354bc944550f9aed3e963de321ede6527125
George Sowers III <Trey@Georges-MacBook-Pro.local>
2016-04-05 14:08:50 -0500

    Edit page styling and bare bones create, edit, and show

--- public/users.show.php
+++ public/users.show.php
@@ -1,4 +1,9 @@
 <?php 
+
+include_once "../bootstrap.php";
+require_once "../utils/Auth.php";
+require_once "../utils/Input.php";
+
 ?>
 
 <!DOCTYPE html>
@@ -10,9 +15,17 @@
 
 		<meta charset="utf-8">
 
+		<link rel="stylesheet" href="/css/main.css">
+
+
 	</head>
 
 	<body>
+
+		<div class='flexbox page_content'>
+			<img src="/img/Luis_Pic/7.png" alt="Luis" style="width:400px;height:400px;"/>
+		<layer id="placeholderlayer"></layer><div id="placeholderdiv"></div>
+
 	</body>
 
 </html>
