89293805ec2514da305c6539d5a67b18b93d5339
George Sowers III <Trey@Georges-MacBook-Pro.local>
2016-04-08 10:25:22 -0500

    quick fixes to navbar, edit, ads, and more

--- public/users.edit.php
+++ public/users.edit.php
@@ -20,43 +20,46 @@ require_once "../bootstrap.php";
 
 	<body>
 		<?php include_once "../views/partials/navbar.php";?>
-		<h1>Edit Profile</h1>
-		  
-		<h3>Personal info:</h3>
+		<div class="page_title"><h1>Edit Profile</h1></div>
+		
+		<div class="questions page_content">  
+			<h3>Personal info:</h3>
 
-		<form method="POST">
-			<label>First Name: </label>
-	        <input type="text" name="First Name" placeholder="Enter First Name">
-		</form><br>
+			<form method="POST">
+				<label>First Name: </label>
+		        <input type="text" name="First Name" placeholder="Enter First Name">
+			</form><br>
 
-		<form method="POST">
-			<label>Last Name: </label>
-	        <input type="text" name="Last Name" placeholder="Enter Last Name">
-		</form><br>
+			<form method="POST">
+				<label>Last Name: </label>
+		        <input type="text" name="Last Name" placeholder="Enter Last Name">
+			</form><br>
 
-		<form method="POST">
-			<label>Email: </label>
-	        <input type="text" name="Email" placeholder="Enter Email Address">
-		</form><br>
+			<form method="POST">
+				<label>Email: </label>
+		        <input type="text" name="Email" placeholder="Enter Email Address">
+			</form><br>
 
-		<h3>Confirm Changes:</h3>
-    		<form method="POST">
-		        <label>Username: </label>
-		        <input type="text" name="username">
-		    </form><br>
+			<h3>Confirm Changes:</h3>
+	    		<form method="POST">
+			        <label>Username: </label>
+			        <input type="text" name="username" placeholder="Enter Username">
+			    </form><br>
 
-		    <form method="POST">
-		        <label>Password: </label>
-		        <input type="password" name="password">
-		    </form>
-		        <input value="Save Changes" type="button">
-            	<input value="Cancel" type="reset">
-    		</form>
+			    <form method="POST">
+			        <label>Password: </label>
+			        <input type="text" name="password" placeholder="Enter Password">
+			    </form><br>
+			    
+			    <form>
+			        <input value="Save Changes" type="button">
+	            	<input value="Cancel" type="reset">
+	    		</form>
 
-    		<div class='flexbox page_content'>
-			<img src="/img/Luis_Pic/7.png" alt="Luis" style="width:400px;height:400px;"/>
-			<layer id="placeholderlayer"></layer><div id="placeholderdiv"></div>
-
+	    		<div class='flexbox page_content'>
+				<img src="/img/Luis_Pic/7.png" alt="Luis" style="width:400px;height:400px;"/>
+				<layer id="placeholderlayer"></layer><div id="placeholderdiv"></div>
+			</div>
 	</body>
 
 </html>
